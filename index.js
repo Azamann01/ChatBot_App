9e283469a8368be0d867caa9ef7695cdc7cf3c55
Tope Wilson <adeniyitopewilson@gmail.com>
2025-03-18 21:30:44 +0000

    Adding functionality to the chat segment.

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
-// Gemini API segment 
+// Gemini API segment
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const API_KEY = "AIzaSyA8GaBf-smt7SfdFNkzUFvlshnRNZYy_hA";
 
 // const genAI = new GoogleGenerativeAI("GEMINI_API_KEY");
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 let messages = {
-    history: [],
-}
+  history: [],
+};
 
-
- async function sendMessage(){
-    
-    const userMessage = document.querySelector(".chatbot-segment input").value;
-    if (userMessage.length){
-        document.querySelector(".chatbot-segment input").value = ""
-        document.querySelector(".chatbot-segment .chat-section").insertAdjacentHTML("beforeend",`
+async function sendMessage() {
+  const userMessage = document.querySelector(".chatbot-segment input").value;
+  if (userMessage.length) {
+    try {
+      document.querySelector(".chatbot-segment input").value = "";
+      document
+        .querySelector(".chatbot-segment .chat-section")
+        .insertAdjacentHTML(
+          "beforeend",
+          `
         <div class="chat-sender">
           <p>${userMessage}</p>
-        </div>`);
+        </div>`
+        );
 
-        const chat = model.startChat(messages);
-        
-          let result = await chat.sendMessage(userMessage);
-          document.querySelector(".chatbot-segment .chat-section").insertAdjacentHTML("beforeend",`
+      const chat = model.startChat(messages);
+
+      let result = await chat.sendMessage(userMessage);
+      document
+        .querySelector(".chatbot-segment .chat-section")
+        .insertAdjacentHTML(
+          "beforeend",
+          `
         <div class="model">
           <p>${result.response.text()}</p>
-        </div>`);
+        </div>`
+        );
 
-        messages.history.push({
-            role: "user",
-            parts: [{ text: userMessage}],
-          });
+      // UPDATING THE HISTORY
+      messages.history.push({
+        role: "user",
+        parts: [{ text: userMessage }],
+      });
 
-        messages.history.push({
-            role: "model",
-            parts: [{ text: result.response.text()}],
-          });
-
-          
-
-         
-
-        
+      messages.history.push({
+        role: "model",
+        parts: [{ text: result.response.text() }],
+      });
+    } catch (error) {
+      document
+        .querySelector(".chatbot-segment .chat-section")
+        .insertAdjacentHTML(
+          "beforeend",
+          `
+        <div class="error-handler">
+          <p>Please try again, later!</p>
+        </div>`
+        );
     }
-   
-   
+  }
 }
-document.querySelector(".chatbot-segment .send-button").addEventListener("click",()=>sendMessage());
-
-
+// INPUT AND SEND MESSAGE SECTION
+document
+  .querySelector(".chatbot-segment .send-button")
+  .addEventListener("click", () => sendMessage());
+// TARGETING OUR ENTER KEY WITH SEND BUTTON
+document
+  .getElementById("input-text")
+  .addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      document.getElementById("send-btn").click();
+    }
+  });
